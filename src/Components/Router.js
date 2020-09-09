f552d26ad6472bda9116ffc8d85068e844eb2ce0
song <songsm5504@gmail.com>
2020-09-09 23:28:01 +0900

    #1.2 Router Setup

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,1 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+              <span>&copy;{new Date().getFullYear()} song</span>
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
